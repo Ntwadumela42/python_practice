4a821f558df36ba664705eabb3e066444470dcaa
Ntwadumela42 <35854422+Ntwadumela42@users.noreply.github.com>
2018-04-26 16:08:45 -0600

    Update reverse-words.py

--- reverse-words.py
+++ reverse-words.py
@@ -7,3 +7,11 @@ reverse_words("double  spaces") # returns  "elbuod  secaps"
 
 
 """
+
+def reverse_words(str):
+  string_length = len(str.split())                         #Calculate the number of words.
+  final = ''
+  for n in range(0, string_length):                        #From range 0 to the calculated length
+    final = final + (string.split()[(n)][::-1]) + ' '      #Adds the reversed word to the final string.
+    
+  return final  
