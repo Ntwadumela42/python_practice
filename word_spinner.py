27518a9221a52097cb0a0b6bfe7347d0e3c3aec9
Ntwadumela42 <35854422+Ntwadumela42@users.noreply.github.com>
2018-05-09 11:13:44 -0600

    Lvl 6 katas are finally becoming easy.

---
+++ word_spinner.py
@@ -0,0 +1,30 @@
+"""
+Write a function that takes in a string of one or more words, and returns the same string, but with all five or more letter words reversed (Just like the name of this Kata). Strings passed in will consist of only letters and spaces. Spaces will be included only when more than one word is present.
+
+
+Examples:
+
+spinWords( "Hey fellow warriors" ) => returns "Hey wollef sroirraw" 
+spinWords( "This is a test") => returns "This is a test" 
+spinWords( "This is another test" )=> returns "This is rehtona test"
+
+"""
+
+# My solution:
+
+def spin_words(sentence):
+    final = ''
+    words = sentence.split()
+    for word in words:
+      if len(word) >= 5:
+        final = final + (word[::-1]) + ' '
+      else:
+        final = final + word + ' '
+    
+
+    return (final.strip())
+    
+    # Best solution:
+    def spin_words(sentence):
+    # Your code goes here
+    return " ".join([x[::-1] if len(x) >= 5 else x for x in sentence.split(" ")])   #Its amazing how compact it is.
